75c7d063a014b5254ede55ad4ce9d0775a983cb1
Navid Vafaei <vafaermn@gmai.com>
2026-06-14 12:01:54 +0330

    initial of project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InvoiceProcess.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
